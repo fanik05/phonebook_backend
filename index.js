c384a1c15562dcd3a6ef29e90afaabdeaf33f31f
fanik05 <fatinanik129@gmail.com>
2022-03-21 12:28:37 +0600

    morgan logger added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
@@ -94,7 +97,6 @@ app.post('/api/persons', (request, response) => {
 })
 
 const PORT = 3001
-
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`)
 })
