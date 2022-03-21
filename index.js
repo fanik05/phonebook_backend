09396a4673aff77b02e0a72ad3b5fe0584fd500a
fanik05 <fatinanik129@gmail.com>
2022-03-21 16:50:05 +0600

    unknown route added

--- index.js
+++ index.js
@@ -100,6 +100,12 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).json({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`)
