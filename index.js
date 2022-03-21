421666d3282a0e21e62d8502251285c0cc109564
fanik05 <fatinanik129@gmail.com>
2022-03-21 17:06:40 +0600

    merge conflict resolved

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('request-body', (req, res) => JSON.stringify(req.body))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
+app.use(express.static('build'))
 
 let persons = [
   {
