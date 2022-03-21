5504b886e92c1b7177a8575adb0025c77a699e0c
fanik05 <fatinanik129@gmail.com>
2022-03-21 13:36:42 +0600

    exercise 3.8 done

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const morgan = require('morgan')
 
 const app = express()
 
+morgan.token('request-body', (req, res) => JSON.stringify(req.body))
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
 
 let persons = [
   {
