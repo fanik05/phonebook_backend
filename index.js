6735d5d89e7564c55656ba8b2a65c6a07ae5ee58
fanik05 <fatinanik129@gmail.com>
2022-03-21 11:15:08 +0600

    excercise 3.4 done

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ app.get('/api/persons/:id', (request, response) => {
   response.json(person)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
