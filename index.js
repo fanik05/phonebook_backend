16cb0873618ea47103122a9f41fb4aedf56393b3
fanik05 <fatinanik129@gmail.com>
2022-03-23 14:07:47 +0600

    fallback port added

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`)
 })
